8e57c8eb44cbbcb1c5791a167d3e93655f79897b
kklldog <kklldog@qq.com>
2020-10-28 01:23:10 +0800

    update

--- interface.js
+++ interface.js
@@ -6,3 +6,56 @@ var user = {
     lastName: "zhou"
 };
 greeter(user);
+function printLable(labeledObj) {
+    console.log(labeledObj.label);
+}
+var myObj = { szie: 10, label: "size 10 obj" };
+printLable(myObj);
+function createSquare(config) {
+    var newSquare = {
+        color: "white", area: 100
+    };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySq = createSquare({ color: "black" });
+var p1 = { x: 10, y: 20 };
+//p1.x = 10;
+var a = [1, 2, 3, 4];
+var ro = [1, 2, 3, 4];
+function createSquare1(config) {
+    return {
+        color: config.color || "red",
+        area: config.width ? config.width * config.width : 20
+    };
+}
+// let sqo =   {
+//     colour: "red",
+// };
+// let mySq1 = createSquare1(
+//     sqo
+// );
+// let mySq1 = createSquare1(
+//     {
+//         colour: "red",
+//         width: 100
+//     } as SquareConfig
+// );
+var squareOptions = { colour: "red", width: 100 };
+var mySquare = createSquare1(squareOptions);
+var mySearch;
+mySearch = function (source, substring) {
+    return 1 == 1;
+};
+mySearch = function (src, sub) {
+    return 1 == 1;
+};
+var myarr;
+myarr = ["1", "2"];
+var mystr = myarr[0];
+// mystr = myarr["1"];
--- interface.ts
+++ interface.ts
@@ -13,3 +13,96 @@ let user = {
 };
 
 greeter(user);
+
+function printLable(labeledObj: { 
+    label: string
+}) {
+    console.log(labeledObj.label);
+}
+
+let myObj =  { szie: 10, label: "size 10 obj" };
+printLable(myObj);
+
+interface SquareConfig {
+    color?: string;
+    width?: number;
+}
+
+function createSquare(config: SquareConfig): { color: string, area: number} {
+    let newSquare = {
+        color:"white", area: 100
+    };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+
+    return newSquare;
+}
+
+let mySq = createSquare({color: "black"});
+
+interface Point {
+    readonly x: number;
+    readonly y: number;
+}
+
+let p1: Point = {x: 10, y: 20};
+
+//p1.x = 10;
+
+let a: number[] = [1, 2, 3, 4];
+let ro: ReadonlyArray<number> = [1,2,3,4];
+
+function createSquare1(config: SquareConfig): {color: string; area: number} {
+    return {
+        color: config.color || "red",
+        area: config.width ? config.width* config.width: 20
+    };
+}
+
+// let sqo =   {
+//     colour: "red",
+// };
+// let mySq1 = createSquare1(
+//     sqo
+// );
+// let mySq1 = createSquare1(
+//     {
+//         colour: "red",
+//         width: 100
+//     } as SquareConfig
+// );
+
+
+let squareOptions = { colour: "red", width: 100 };
+let mySquare = createSquare1(squareOptions);
+
+interface SearchFunc {
+    (source: string, substring: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function (source:string, substring: string):boolean {
+    return 1==1;
+}
+
+mySearch = function (src, sub){
+    return 1==1;
+}
+
+// mySearch = function (src, sub) {
+//     return "string";
+// }
+
+interface StringArray {
+    [a: number]: string;
+}
+
+let myarr : StringArray;
+myarr = ["1" , "2"];
+
+let mystr : string = myarr[0];
+// mystr = myarr["1"];
